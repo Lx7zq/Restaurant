ee7433a348afe2df607c9c31753568c41c4e1029
lx7zq <thisforworkonly7@gmail.com>
2024-06-27 17:32:23 +0700

    27/06

--- src/Component/header.jsx
+++ src/Component/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className=" font-semibold text-2xl">
-      <span>Grab Restaurant</span>
+      <span>Grab Restaurantttttt</span>
     </header>
   );
 };
